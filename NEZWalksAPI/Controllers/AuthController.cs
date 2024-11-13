60c717f71e6d07824887ff4d6c4445db45b0ff99
lirak.xhelili <lirak.xhelili@tretek.io>
2024-11-13 14:45:09 +0100

    Create Login Action Method

--- NEZWalksAPI/Controllers/AuthController.cs
+++ NEZWalksAPI/Controllers/AuthController.cs
@@ -44,5 +44,26 @@ namespace NEZWalksAPI.Controllers
             }
             return BadRequest("Something went wrong");
         }
+
+        //LOGIN 
+        //POST: /api/Auth/Login
+        [HttpPost]
+        [Route("Login")]
+
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginRequestDto.Username);
+            if (user != null)
+            {
+                var checkPasswordResult = await userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+                if (checkPasswordResult)
+                {
+                    //Create token
+                    return Ok();
+                }
+            }
+            return BadRequest("Username or password incorrect!!");
+        }
     }
 }
